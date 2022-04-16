5eef2b11a2c307329b8690d41e7d4585929e68f0
weicheng <qihengsan1129@163.com>
2022-04-16 22:21:23 +0800

    first commit

---
+++ src/main/java/com/cheng/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.cheng.git;
+
+/**
+ * @Author: weicheng
+ * @Date: 2022/4/16 22:15
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println(("hello git!"));
+    }
+}
